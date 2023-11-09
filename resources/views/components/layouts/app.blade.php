63562e235d77e184f1c9076384f30a758de2c93b
hector <lariosh69@gmail.com>
2023-11-09 16:21:01 -0600

    [UPDATE][HECTOR] Se instala tailwind y se elimina bootstrap y sass

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ $title ?? 'PF | DW' }}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Default meta description' }}">
 </head>
