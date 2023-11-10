b2d027938e24d332d83832be21b86746f9e9c986
FranciscoJRO <francisco.reynoso2000@gmail.com>
2023-11-10 00:44:39 -0600

    Refactor form styles and button classes

--- resources/views/comments/comment_form.blade.php
+++ resources/views/comments/comment_form.blade.php
@@ -1,12 +1,12 @@
 <div class=" container-fluid mb-4">
     <div class="row mb-2">
-        <textarea name="body" class="form-control" placeholder="Deja tu comentario aquí..." id="floatingTextarea2"
+        <textarea name="body" class="form-control border p-3 w-full rounded-lg @error('name') border-red-400 @enderror" placeholder="Deja tu comentario aquí..." id="floatingTextarea2"
             style="height: 100px"></textarea>
         @error('body')
             <p style="color:red">{{ $message }}</p>
         @enderror
     </div>
     <div class="row">
-        <button type="submit" class="btn btn-neon cian-text btn-lg">Envíar</button>
+        <button type="submit" class=" mt-5 bg-emerald-400 hover:bg-emerald-600 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" type="button">Envíar</button>
     </div>
 </div>
--- resources/views/posts/forms.blade.php
+++ resources/views/posts/forms.blade.php
@@ -1,30 +1,42 @@
-<label>
-    Titulo <br>
-    <input name="title" type="text" value="{{ old('title', $post->title) }}">
-    @error('title')
-        <br>
-        <small style="color:red">{{ $message }}</small>
-    @enderror
-</label><br>
-<label>
-    Descripcion <br>
-    <textarea name="body">{{ old('body', $post->body) }}</textarea>
-    @error('body')
-        <br>
-        <small style="color:red">{{ $message }}</small>
-    @enderror
-</label><br>
-<label for="category_id">Categoría:</label>
+<div class="mb-5">
+    <label  class="mb-2 block uppercase text-gray-500 font-bold">
+        Titulo
+    </label>
+        <input name="title" type="text" value="{{ old('title', $post->title) }}" placeholder="Titulo de tu post"
+        class="border p-3 w-full rounded-lg @error('name') border-red-400 @enderror" >
+        @error('title')
+        <p class="bg-red-500 text-white my-2 rounded-lg text-sm text-center p-2">
+            {{ $message }}
+        </p>
+        @enderror
+    <br>
+</div>
+<div class="mb-5">
+    <label class="mb-2 block uppercase text-gray-500 font-bold">
+        Descripcion <br>
+        <textarea name="body" value="{{ old('body', $post->body) }}" 
+        class="border p-3 w-full rounded-lg @error('name') border-red-400 @enderror"> 
+        </textarea>
+        @error('body')
+        <p class="bg-red-500 text-white my-2 rounded-lg text-sm text-center p-2">
+            {{ $message }}
+        </p>
+        @enderror
+    </label><br>
+</div>
+
+<label for="category_id" class="mb-2 block uppercase text-gray-500 font-bold">Categoría:</label>
 <select name="category_id" id="category_id">
     @foreach ($categories as $category)
-        <option value="{{ $category->id }}">{{ $category->category }}</option>
+        <option  value="{{ $category->id }}">{{ $category->category }}</option>
     @endforeach
 </select>
-<label>
-    Imagen <br>
+<label >
+    <div class="mb-2 block uppercase text-emerald-400 font-bold">Seleciona una imagen que quieras subir a Finstagram </div> <br>
     <input type="file" name="image">
     @error('image')
         <br>
-        <small style="color:red">{{ $message }}</small>
+        <p class="bg-red-500 text-white my-2 rounded-lg text-sm text-center p-2">
+            {{ $message }}
     @enderror
 </label>
