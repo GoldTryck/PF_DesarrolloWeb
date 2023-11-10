5212bbbfe820d2b4c6512b7f2afc16708ee4707e
FranciscoJRO <francisco.reynoso2000@gmail.com>
2023-11-10 02:52:20 -0600

    Add font-awesome library and update UI icons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,20 +5,22 @@
     @endsection
 
     @section('contenido')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
         <div class="flex  justify-left items-center">
             <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
                 <div class="md:w-8/12 lg:w-6/12 px-5">
                     <img class="mx-auto" src="{{ asset('img/usuario.svg') }}" alt="Imagen de perfil del usuario">
                 </div>
             </div>
-            <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-                <div class="md:w-8/12 lg:w-6/12 px-5">
-                    <a href="{{ route('posts.create') }}"
-                        class="bg-emerald-400 hover:bg-emerald-600 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-full">Crear
-                        post</a>
-                </div>
+            <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+                <a href="{{ route('posts.create') }}"
+                    class="bg-emerald-400 hover:bg-emerald-600 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-full text-center">
+                    <i class="icon-create-post fas fa-plus"></i>  Crear un post</a>
+                <a href="{{ route('posts.by_author')}}"
+                    class="bg-emerald-400 hover:bg-emerald-600 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-full text-center">
+                    <i class="icon-home fas fa-home"></i> Feed de Finstagram</a>
             </div>
         </div>
-
+      
 
     </x-layouts.app>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,11 +1,14 @@
 <x-layouts.app
+
     title="Nuevo post"
     meta-description="Formulario para la elaboracion de un nuevo post"
     header="CREAR NUEVO POST">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
     <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         @include('posts.forms')
-        <button class= " mt-5 bg-emerald-400 hover:bg-emerald-600 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" type="submit">Send</button>
+        <button class= " mt-5 bg-emerald-400 hover:bg-emerald-600 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" type="submit">
+            <i class="fas fa-paper-plane"></i> Send</button>
     </form>
 </x-layouts.app>
