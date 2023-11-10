7996722c35b2a8e3f89a169b04be33e6b03f7711
FranciscoJRO <francisco.reynoso2000@gmail.com>
2023-11-10 00:46:43 -0600

    Add top margin to comment form textarea

--- resources/views/comments/comment_form.blade.php
+++ resources/views/comments/comment_form.blade.php
@@ -1,6 +1,6 @@
 <div class=" container-fluid mb-4">
     <div class="row mb-2">
-        <textarea name="body" class="form-control border p-3 w-full rounded-lg @error('name') border-red-400 @enderror" placeholder="Deja tu comentario aquí..." id="floatingTextarea2"
+        <textarea name="body" class=" mt-5 form-control border p-3 w-full rounded-lg @error('name') border-red-400 @enderror" placeholder="Deja tu comentario aquí..." id="floatingTextarea2"
             style="height: 100px"></textarea>
         @error('body')
             <p style="color:red">{{ $message }}</p>
