74aaf77a0785aa76ec8bd135c27f30456255fe32
FranciscoJRO <francisco.reynoso2000@gmail.com>
2023-11-10 10:32:11 -0600

    Add profile picture and description fields to about and dashboard views

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,17 @@
     title="FI | ABOUT"
     meta-description="Descrición del Home"
     header="SOBRE NOSOTROS">
+    <div class="form-group">
+        <label for="foto_perfil" class="mb-2 block uppercase text-gray-500 font-bold">Foto de Perfil:</label>
+        <input type="file" name="foto_perfil" id="foto_perfil">
+    </div>
+<br>
+    <div class="form-group">
+        <label for="descripcion"class="mb-2 block uppercase text-gray-500 font-bold" >Descripción:</label>
+        <textarea class=" text-white my-2 rounded-lg text-sm text-center p-2" name="descripcion" id="descripcion" rows="4"></textarea>
+    </div>
+
+    <button type="submit" class="mt-5 bg-emerald-400 hover:bg-emerald-600 transition-colors cursor-pointer uppercase font-bold w-auto p-3 text-white rounded-lg">Guardar</button>
+
 
 </x-layouts.app>
